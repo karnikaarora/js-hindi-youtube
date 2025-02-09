c06cceb775d1c1e373198370e8d1be9e055f5b85
Karnika <149869274+karnikaarora@users.noreply.github.com>
2025-02-09 10:36:54 +0000

    final

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,5 @@
-const name = "hitesh"
+const name = "hitesh"  
+//string
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
@@ -6,6 +7,7 @@ const repoCount = 50
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc-com')
+//String object
 
 // console.log(gameName[0]);
 console.log(gameName.__proto__);
@@ -13,23 +15,35 @@ console.log(gameName.__proto__);
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
+
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
 const newString = gameName.substring(0, 4)
 console.log(newString);
+//The substring(start, end) method extracts characters from index start to end - 1.
+// Does not support negative indexes.
+// If end is less than start, it swaps them.
 
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
+// slice(start, end) extracts characters from start to end - 1.
+// Supports negative indexes, where -1 refers to the last character.
 
 const newStringOne = "   hitesh    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
+//.trim() removes leading and trailing spaces (but not spaces inside the string).
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
 console.log(url.replace('%20', '-'))
+//.replace(oldValue, newValue) replaces the first occurrence of oldValue with newValue.
 
 console.log(url.includes('sundar'))
+// .includes(value) checks if value exists in the string.
+// Returns true if found, otherwise false.
 
 console.log(gameName.split('-'));
+//.split(separator) splits the string into an array based on the given separator.
+//returns an array 
--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,22 +1,30 @@
 const score = 400
-// console.log(score);
+console.log(score);
+//primitive number type
 
-const balance = new Number(100)
-// console.log(balance);
+const balance = new Number(100) 
+console.log(balance);
+//number object
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+console.log(balance.toString().length);
+//converts into string
+console.log(balance.toFixed(1));
+//.toFixed(1) rounds the number to 1 decimal place.
 
 const otherNumber = 123.8966
-
-// console.log(otherNumber.toPrecision(4));
+console.log(otherNumber.toPrecision(4));
+//.toPrecision(n) formats a number with n significant digits.
+//123.9
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
-
+console.log(hundreds.toLocaleString('en-IN'));
+//.toLocaleString('en-IN') formats the number using Indian number system (lakhs, crores).
+//10,00,000
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
-// console.log(Math);
+console.log(Math);
+//built-in JavaScript object
+
 // console.log(Math.abs(-4));
 // console.log(Math.round(4.6));
 // console.log(Math.ceil(4.2));
@@ -25,10 +33,18 @@ const hundreds = 1000000
 // console.log(Math.max(4, 3, 6, 8));
 
 console.log(Math.random());
+// generates a random decimal between 0 and 1.
+
 console.log((Math.random()*10) + 1);
+// generates a random decimal between 0 and 10.
+//when *(num) upper limit is that num 
+
 console.log(Math.floor(Math.random()*10) + 1);
+//converts the prev result into a whole number
 
 const min = 10
 const max = 20
 
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+//*(max-min+1) means lower limit = min and upper limit = max
+//and math.floor to get a whole number 
